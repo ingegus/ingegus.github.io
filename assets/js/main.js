3e176b7f8d5d4f867b509176073eef0314a72834
Gustavo Adolfo Rodríguez Alonso <gustavo.rodriguez@hotmail.es>
2024-10-25 15:08:27 -0500

    validación CSS, ajuste de footer y js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,115 +1,106 @@
-/*---- Feed RSS ----*/
-const mediumUrl = 'https://ingegus.medium.com'; // URL de Medium
-const numPostsToShow = 6; // Número de posts a mostrar
-// Construir la URL del feed de Medium
-const feedUrl = `https://api.rss2json.com/v1/api.json?rss_url=${mediumUrl}/feed`;
-// Función para crear las tarjetas
-function createCard(item, index) {
-    // Obtener la primera imagen del contenido
-    const firstImage = $(item.content).find('img').first().attr('src');
-    const thumbnail = firstImage ? firstImage : 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Medium_Logo.webp'; // Si no hay imagen, usar una imagen de marcador de posición
+$(document).ready(function () {
+    /*---- Feed RSS ----*/
+    const mediumUrl = 'https://ingegus.medium.com';
+    const numPostsToShow = 6;
+    const feedUrl = `https://api.rss2json.com/v1/api.json?rss_url=${mediumUrl}/feed`;
 
-    const card = `
+    // Crear tarjetas de feed RSS
+    function createCard(item, index) {
+        const firstImage = $(item.content).find('img').first().attr('src');
+        const thumbnail = firstImage || 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Medium_Logo.webp';
+
+        return `
             <div class="col-md-4 mb-4">
                 <div class="blog__card h-100">
-                    <img src="${thumbnail}" class="blog__card--img " alt="Imagen del artículo ${index + 1}">
+                    <img src="${thumbnail}" class="blog__card--img" alt="Imagen del artículo ${index + 1}">
                     <div class="blog__card--body">
                         <h4 class="blog__card--title">${item.title}</h4>
                         <a href="${item.link}" target="_blank" rel="noopener noreferrer" class="btn btn-bd-primary mt-auto">Leer más</a>
                     </div>
                 </div>
-            </div>
-        `;
+            </div>`;
+    }
 
-    return card;
-}
-// Función para cargar y mostrar las tarjetas
-function loadRssCards() {
-    $.getJSON(feedUrl, function (data) {
-        if (data.status === 'ok') {
-            const items = data.items.slice(0, numPostsToShow);
+    // Crear indicadores del carrusel
+    function createIndicators(numSlides) {
+        let indicatorsHtml = '';
+        for (let i = 0; i < numSlides; i++) {
+            indicatorsHtml += `<li data-bs-target="#carouselExampleControls" data-bs-slide-to="${i}" class="${i === 0 ? 'active' : ''}" aria-label="Slide ${i + 1}"></li>`;
+        }
+        return indicatorsHtml;
+    }
 
-            const rssCardsContainer = $('#rssCards');
-            let html = '';
-            let indicatorsHtml = '';
-            let active = 'active';
-            for (let i = 0; i < items.length; i += 3) {
-                html += `<div class="carousel-item ${active}"><div class="row">`;
-                for (let j = i; j < i + 3 && j < items.length; j++) {
-                    const cardHtml = createCard({
-                        title: items[j].title,
-                        content: items[j].content,
-                        link: items[j].link,
-                        guid: j,
-                    }, j);
-                    html += cardHtml;
+    // Cargar y mostrar tarjetas RSS
+    function loadRssCards() {
+        $.getJSON(feedUrl)
+            .done(function (data) {
+                if (data.status === 'ok') {
+                    const items = data.items.slice(0, numPostsToShow);
+                    const rssCardsContainer = $('#rssCards');
+                    let html = '';
+                    for (let i = 0; i < items.length; i += 3) {
+                        html += `<div class="carousel-item ${i === 0 ? 'active' : ''}"><div class="row">`;
+                        for (let j = i; j < i + 3 && j < items.length; j++) {
+                            html += createCard(items[j], j);
+                        }
+                        html += '</div></div>';
+                    }
+                    rssCardsContainer.html(html);
+                    $('#carouselIndicators').html(createIndicators(Math.ceil(items.length / 3)));
+                    if (items.length <= 3) $('.carousel-control-prev, .carousel-control-next').hide();
                 }
-                html += '</div></div>';
-                indicatorsHtml += `<li data-bs-target="#carouselExampleControls" data-bs-slide-to="${i / 3}" class="${active}" aria-label="Slide ${i / 3 + 1}"></li>`;
-                active = '';
-            }
-            rssCardsContainer.html(html);
-            $('#carouselIndicators').html(indicatorsHtml);
+            })
+            .fail(function () {
+                console.error("Error al cargar el feed RSS.");
+            });
+    }
 
-            // Ocultar los controles si hay 3 o menos posts
-            if (items.length <= 3) {
-                $('.carousel-control-prev, .carousel-control-next').hide();
-            }
-        }
-    });
-}
-// Cargar y mostrar las tarjetas al cargar la página
-$(document).ready(function () {
     loadRssCards();
-});
-/*--------------------------------------*/
-/*--- Filtro de la sección portafolio---*/
-$(document).ready(function () {
 
+    /*--- Filtro de la sección portafolio ---*/
     $(".filter-b").click(function () {
-        var value = $(this).attr('data-filter');
-        if (value == "todo") {
-            $('.filter').show('1000');
-        }
-        else {
-            $(".filter").not('.' + value).hide('3000');
-            $('.filter').filter('.' + value).show('3000');
-        }
-        // Agrega la clase active al botón seleccionado
+        const value = $(this).attr('data-filter');
+        $('.filter').toggle(value === 'todo').filter(`.${value}`).show('9000');
         $(".filter-b").removeClass("active");
         $(this).addClass("active");
     });
+
+    /*--- Botón ir hacia arriba ---*/
+    $('.go-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 300);
+    });
+
+    $(window).scroll(function () {
+        $('.go-to-top').toggle($(this).scrollTop() > 600);
+    });
+
+    /*--- Año actual ---*/
+    $('#year').text(new Date().getFullYear());
+
+    /*--- Activar clase en menú ---*/
+    function activateSectionOnScroll() {
+            var scrollPosition = $(window).scrollTop();
+    
+            $('.section').each(function () {
+                var sectionOffset = $(this).offset().top;
+                var sectionHeight = $(this).outerHeight();
+                var sectionId = $(this).attr('id');
+                var $link = $('a[href="#' + sectionId + '"]')
+            });
+        }
+               
+        // Al hacer clic en un enlace, activa la sección correspondiente y la clase
+        $('.nav-link').on('click', function (e) {
+            e.preventDefault();
+    
+            var target = $(this).attr('href');
+            var offset = $(target).offset().top;
+    
+            $('html, body').animate({ scrollTop: offset }, 500, function () {
+                $('.nav-link').removeClass('nav-link--active');
+                $(e.target).addClass('nav-link--active');
+                
+                
+            });
+        });          
 });
-/*-----------------------------*/
-/*--- Botón ir hacia arriba ---*/
-
-$(document).ready(function(){
-
-	$('.go-to-top').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 600 ){
-			$('.go-to-top').slideDown(300);
-		} else {
-			$('.go-to-top').slideUp(300);
-		}
-	});
-
-});
-
-/*----------------*/
-/*---Año actual---*/
-
-$(document).ready(function() {
-    var currentYear = new Date().getFullYear();  // Obtiene el año actual
-    $('#year').text(currentYear);  // Inserta el año en el elemento con id "year"
-});
-
-
-/*--------------------------------------------------*/
-/*---- Enlaces activos al hacer clic en el menú ----*/
-
